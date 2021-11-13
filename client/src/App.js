0d60e850510ac8e414b673eafcdd0a727925c573
Rohit-Parameswaran <rohitparameswaran1911@gmail.com>
2021-11-13 23:45:22 +0530

    added backend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FileUpload from './components/FileUpload'
+
+function App() {
+  return (
+    <div className="container mx-auto py-4">
+      <FileUpload/>
+    </div>
+  );
+}
+
+export default App;
